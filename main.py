a6353708a841cebf76a4c463336aa8424adbb3b9
Michael Warmbier <MichaelCWarmbier@gmail.com>
2022-06-03 17:28:06 +0000

    feat: - Improved error handling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#### IMPORTS ####
+
 import discord   
 import requests   
 import os
@@ -5,17 +7,32 @@ import json
 import hashlib
 from datetime import datetime
 
-client = discord.Client() # Initialize client 
-with open('server.json', 'r') as f:
-    server = json.loads(f.read())
+#### DATA ####
 
-# ANSI colors
-class t_colors:
+class t_colors: # ANSI colors
     NAME = '\033[94m'
     INFO = '\033[92m'
     NOCOLOR = '\033[0m'
+    ERR = '\033[91m'
 
-### Utility Methods ###
+helpMessage = '```$help```\nInstructions on each command\n\n```$ping```\nShows bot information and status\n\n```$updatePrefs <preference> <value>```\nAllows you to set specific preferences for easier use\n\nPreferences: *privacy, zipcode, country, units*\nPrivacy options: *public, private*\nUnit options: *imperial, metric*\n\nDM me to update your settings in private!\n\n```$weather <zipcode> <countrycode> <units>``` \nOR\n ```$weather <zipcode> <countrycode>``` \nOR\n ```$weather // only works with set preferences```\n\nGet current weather data. Default units are metric.\nList of country codes: https://www.iso.org/obp/ui/#search'
+
+with open('server.json', 'r') as f:
+    server = json.loads(f.read())
+
+#################################################
+
+try:
+  client = discord.Client() # Initialize client 
+except:
+  throwErr('Unable to start client.')
+
+#################################################
+
+
+
+
+#### UTILITY METHODS ####
 
 async def updateServerInfo(what, title, pref, data):
   title = await hash(str(title))
@@ -39,15 +56,23 @@ async def updateServerInfo(what, title, pref, data):
 
 async def hash(str):
   return (hashlib.md5(str.encode()).hexdigest());
-    
-### Command Methods ###
+
+def throwErr(msg):
+  print(t_colors.ERR + msg)
+  return msg
+
+
+
+
+
+  
+#### COMMAND METHODS ####
 
 async def getWeatherViaZip(zipcode, countrycode, units, privacy):
 
   # If units of measurement is invalid, return with error
   if units.lower() != 'imperial' and units.lower() != 'metric':
-    return 'Invalid unit of measurement'
-    raise Exception("Invalid unit of measurement")
+    return throwErr("Error: Invalid unit of measurement")
 
   # Request information and store response as json data
   response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',' + countrycode + '&appid=' +os.environ['API_KEY'] + '&units=' + units)
@@ -55,8 +80,7 @@ async def getWeatherViaZip(zipcode, countrycode, units, privacy):
 
   # If data.cod is 404, something went wrong
   if data['cod'] == '404':
-    return 'Incorrect information. Use $help for more info.'
-    raise Exception("Incorrect information")
+    return throwErr('Error: Incorrect information. Use $help for more info.')
 
  
   # Store data
@@ -77,10 +101,11 @@ async def getWeatherViaZip(zipcode, countrycode, units, privacy):
   # Return results
   return str(f"Weather: {weather}\nTemperature: {temp}°{unit}\nFeels like: {feelslike}°{unit}\nHumidity: {humid}%\nRange: {min}°{unit} - {max}°{unit}\nLocation: {location}")
 
+  
 
 
-
-
+  
+#### CLIENT METHODS ####
 
 @client.event
 async def on_ready(): # When bot is turned on:
@@ -102,58 +127,71 @@ async def on_message(message): # When a message is sent:
   
   # Returns message showing that the bot is currently working
   if message.content.startswith('$ping'):
-    await message.channel.send('Discord Weather Bot\nCreated by Michael Warmbier\nSource: https://replit.com/@Kirbout/Discord-Weather-Bot')
+    try:
+      await message.channel.send('Discord Weather Bot\nCreated by Michael Warmbier\nSource: https://replit.com/@Kirbout/Discord-Weather-Bot')
+    except:
+      throwErr('Error: Please report this bug to the developer.')
 
   # Returns message showing instructions on how to use each command
   if message.content.startswith('$help'):
-      await message.channel.send('```$help```\nInstructions on each command\n\n```$ping```\nShows bot information and status\n\n```$updatePrefs <preference> <value>```\nAllows you to set specific preferences for easier use\n\nPreferences: *privacy, zipcode, country, units*\nPrivacy options: *public, private*\nUnit options: *imperial, metric*\n\nDM me to update your settings in private!\n\n```$weather <zipcode> <countrycode> <units>``` \nOR\n ```$weather <zipcode> <countrycode>``` \nOR\n ```$weather // only works with set preferences```\n\nGet current weather data. Default units are metric.\nList of country codes: https://www.iso.org/obp/ui/#search')
+      try:
+        await message.channel.send(helpMessage)
+      except:
+        await message.channel.send(throwErr('Error: Please report this bug to the developer.'))
 
   # Retrieves weather based on location
   if message.content.startswith('$weather'):
-    
+
+    # Check to see length of arguments; is the user going off their saved preferences?
     command = (message.content).split(' ')
-    if len(command) == 4:
-      result = await getWeatherViaZip(command[1], command[2], command[3], 'public')
-      await message.channel.send(result)
-    elif len(command) == 3:
-      result = await getWeatherViaZip(command[1], command[2], 'metric', 'public')
-      await message.channel.send(result)
-    elif len(command) == 1:
-      global server
-      user = await hash(str(user))
-      if user in server['userPref']:
-        zipcode = server['userPref'][user]['zipcode']
-        country = server['userPref'][user]['country']
-        privacy = server['userPref'][user]['privacy']
-        units = server['userPref'][user]['units']
+    try:
+      if len(command) == 4:
+        result = await getWeatherViaZip(command[1], command[2], command[3], 'public')
+        await message.channel.send(result)
+      elif len(command) == 3:
+        result = await getWeatherViaZip(command[1], command[2], 'metric', 'public')
+        await message.channel.send(result)
+      elif len(command) == 1:
+        global server
+        user = await hash(str(user))
+        if user in server['userPref']:
+          zipcode = server['userPref'][user]['zipcode']
+          country = server['userPref'][user]['country']
+          privacy = server['userPref'][user]['privacy']
+          units = server['userPref'][user]['units']
 
-      if zipcode == '0':
-        await message.channel.send('User has no postal/zip code!')
-        raise Exception("Missing zipcode")
+        if zipcode == '0':
+          await message.channel.send(throwErr('Error: User is missing a zipcode.'))
 
-      result = await getWeatherViaZip(zipcode, country, units, privacy)
-      await message.channel.send(result)
-      return
-    else:
-      await message.channel.send('Invalid number of arguments')
-      raise Exception("Invalid number of arguments")
+        result = await getWeatherViaZip(zipcode, country, units, privacy)
+        await message.channel.send(result)
+        return
+      else:
+        await message.channel.send(throwErr("Error: Invalid number of arguments."))
+    except:
+      await message.channel.send(throwErr('Error: Unable to retrieve weather info. Information may be incorrect.'))
 
+  # Update specific preferences
   if message.content.startswith('$updatePrefs'):
     command = (message.content).split(' ')
+    
     if len(command) == 3:
       if ((str(command[2]) == 'private' or str(command[2]) == 'public') and str(command[1]) == 'privacy') or (str(command[1]) == 'zipcode') or (str(command[1]) == 'country') or (str(command[1]) == 'units' and (str(command[2]) == 'metric' or str(command[2]) == 'imperial')):
         await updateServerInfo('userPref', user, command[1], command[2])
         await message.channel.send('Updated preferences successfully')
       else:
-        await message.channel.send('Invalid argument')
-        raise Exception("Invalid argument")
-    else:
-      await message.channel.send('Invalid number of arguments')
-      raise Exception("Invalid number of arguments")
-
+        await message.channel.send(throwErr("Error: Invalid argument."))
         
+    else:
+      await message.channel.send(throwErr("Error: Invalid number of arguments."))
+
+
+
+
+
       
-    
+#################################################
 
 client.run(os.environ['TOKEN']) # Start bot
- 
+
+#################################################
